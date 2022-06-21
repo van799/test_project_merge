e0fb9a35f19bedfe0ea0d2a825ae8b3a439b68f1
van799 <73700620+van799@users.noreply.github.com>
2022-06-21 19:57:42 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+
+# здесь объявляется класс StreamData
+class StreamData:
+    def create(self, FIELDS, lst_values):
+
+        if len(FIELDS) == len(lst_values):
+            self.__dict__= dict(zip(FIELDS, lst_values))
+            return True
+        else:
+            return False
+
+class StreamReader:
+    FIELDS = ('id', 'title', 'pages')
+
+    def readlines(self):
+        lst_in = list(map(str.strip, sys.stdin.readlines()))  # считывание списка строк из входного потока
+        sd = StreamData()
+        res = sd.create(self.FIELDS, lst_in)
+        return sd, res
+
+
+sr = StreamReader()
+data, result = sr.readlines()
